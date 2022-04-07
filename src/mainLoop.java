8d7a2219adb3e4c9458953e162b070b48e3d3714
n00bady <kaz00@vivaldi.net>
2022-04-08 02:50:07 +0300

    Added some more rooms and stuff to play around. Modified the Door class partial constructor.

--- src/mainLoop.java
+++ src/mainLoop.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class mainLoop {
     // TODO: More commands, fix the exceptions, seperate the displaying of items/things etc from
     //      the displayRoom() method. Add an actual smoll scenario instead of just example cases!
+    //      better selection recognition for playerInput() .
 
     private Room rooms[];
     private Player p1 = new Player();
@@ -39,26 +40,41 @@ public class mainLoop {
     public void initWorld() {
 
         // Room creation
-        rooms = new Room[3];
+        rooms = new Room[5];
         rooms[0] = new Room("Starting room.", "You start here.");
         rooms[1] = new Room("Second room.", "An empty room.");
         rooms[2] = new Room("Exit room", "You found the exit.");
+        rooms[3] = new Room("An empty room", "A large empty room.");
+        rooms[4] = new Room("Restroom", "A very dirty and dillapitated restroom. Yuck!");
 
         // Keys creation
         Item key1 = new Item ("Key", "An small shiny key.");
         // Doors creation
+        // !!!DO NOT put . at the names of your doors it's throwing off the use command logic!!!
         Door door1 = new Door("East Door", "An simple door.", new Exit(Exit.EAST, rooms[2]), true, key1);
+        Door door2 = new Door("Restroom door", "Humans only! says on the door.", new Exit(Exit.EAST, rooms[4]));
+        Door door3 = new Door("Restroom door", "Humans only! says on the door.", new Exit(Exit.WEST, rooms[0]));
 
         // Exits for each room if they have a door then the exit is added in the door.(a little stupid way of doing it)
         rooms[0].addExit(new Exit(Exit.NORTH, rooms[1])); // start room exits
+        rooms[0].addExit(new Exit(Exit.WEST, rooms[3]));
+        rooms[0].addDoor(door2);
+
         rooms[1].addExit(new Exit(Exit.SOUTH, rooms[0])); // second room exits
         rooms[1].addDoor(door1); // the door contains the exit for this place
+
+        rooms[3].addExit(new Exit(Exit.EAST, rooms[0]));
+
+        rooms[4].addDoor(door3);
         // last room doesn't have any exits
 
         // add items in rooms
         rooms[0].addItem(key1);
         rooms[0].addItem(new Item("Teddy Bear", "A small cute teddy bear."));
         rooms[0].addThing(new Thing("Broken Statue", "A very old broken statue of some long forgotten deity."));
+
+        rooms[4].addThing(new Thing("Bathroom stalls", "Some very dirty bathroom stalls, just from the smell you do not even want to look inside!"));
+        rooms[4].addThing(new Thing("Sink", "A small dirty sink, broken on the left corner. Hope you are not thirsty..."));
 
         // player starting position
         p1.setRoom(rooms[0]);
@@ -217,13 +233,13 @@ public class mainLoop {
                                 }
                             } else {
                                 // just go through if it's unlocked
-                                System.out.println("You go throug the " + a_door.getName() + "...");
+                                System.out.println("You go through the " + a_door.getName() + "...");
                                 p1.setRoom(a_door.getExit().getLeadsTo());
                                 success =true;
                             }
                         }
                     }
-                    if (!success) { System.out.println("You can't go throug!"); }
+                    if (!success) { System.out.println("You can't go through!"); }
                     Thread.sleep(slowdown);
                     break;
                 case "QUIT":
