62fd577905f32ee8e97bf520e02f5f33bb8598f8
n00bady <kaz00@vivaldi.net>
2022-04-08 00:46:59 +0300

    Some small fixes, updates and comments. Moved all the scanning for commands in playerInput(). Also move the quit command insiede the playerInput(). Resolved the out of bounds exception when the player give only one word as input by checking if there are more than one words in the input string. Other smaller touch ups. :^)

--- src/mainLoop.java
+++ src/mainLoop.java
@@ -1,18 +1,16 @@
 import java.util.Enumeration;
 import java.util.Scanner;
 
-
 public class mainLoop {
-    // TODO: Need to separate possibly player input on it's own method also
-    //      need to support verbs (parser?) for actions like go, look, pickup etc...
+    // TODO: More commands, fix the exceptions, seperate the displaying of items/things etc from
+    //      the displayRoom() method. Add an actual smoll scenario instead of just example cases!
 
     private Room rooms[];
     private Player p1 = new Player();
-
+    private int slowdown = 1500; // Used to simulate the time it takes to do an action
+    boolean quit = false;
 
     public void startLoop() {
-        boolean quit = false;
-
         // init player
         System.out.println("Choose your name: ");
         Scanner input = new Scanner(System.in);
@@ -27,19 +25,11 @@ public class mainLoop {
             displayRoom();
 
             // get player input
-            System.out.print(">> ");
-            String cmd = input.nextLine();
-            cmd = cmd.toUpperCase();
-            //System.out.println(cmd);
-            playerInput(cmd);
+            playerInput();
 
             // winning condition
             if (p1.getRoom() == rooms[2]) {
                 System.out.println("Congrats you found the exit!");
-                quit = true;
-            }
-            // exit condition
-            if (cmd.equals("QUIT") || cmd.equals("Q")) {
                 quit = true;
             }
         } while (!quit);
@@ -113,104 +103,142 @@ public class mainLoop {
     }
 
     //Player input
-    public void playerInput(String cmd) {
-        boolean success = false;
-        try {
-            // verb
-            String command = cmd.split(" ")[0];
-            // noun
-            String selection = cmd.split(" ", 2)[1];
-            System.out.println("Used command: " + command);
-            System.out.println("Selection: " + selection);
-            System.out.println();
+    public void playerInput() {
+        boolean success = false;  // probably there is a better way, but this is what I could do...
+        Scanner input = new Scanner(System.in);
 
+        System.out.print(">> ");
+        String cmd = input.nextLine();
+        cmd = cmd.toUpperCase();
+
+        // verb
+        String command = cmd.split(" ")[0];
+        // noun(s)
+        String selection = " ";
+        // Check if user's input is more than one word
+        // we do this incase there is only a command otherwise we get out of bounds exceptions :P
+        if (cmd.indexOf(" ") > 0) {
+            selection = cmd.split(" ", 2)[1];
+        }
+        System.out.println("Used command: " + command);
+        System.out.println("Selection: " + selection);
+        System.out.println();
+
+        // Switch for commands starts here ---
+        try {
             switch (command) {
                 case "GO":
-                    //System.out.println("Go Command selected.");
+                    // check if exit exists then move player
                     for (Enumeration e = p1.getRoom().getExits().elements(); e.hasMoreElements(); ) {
                         Exit an_exit = (Exit) e.nextElement();
                         if ((an_exit.getFullDirectionName().compareTo(selection) == 0) ||
                                 an_exit.getShortDirectionName().compareTo(selection) == 0) {
                             System.out.println("You move to the " + an_exit.getFullDirectionName() + " room...");
-                            Thread.sleep(1500);
                             p1.setRoom(an_exit.getLeadsTo());
                             success = true;
                         }
                     }
-                    if (!success) { System.out.println("You can't go that way!"); }
+                    if (!success) {
+                        System.out.println("You can't go that way!");
+                    }
+                    Thread.sleep(slowdown);
                     break;
                 case "TAKE":
-                    // add item to inventory
+                    // add item to inventory and remove it from the room
                     for (Enumeration i = p1.getRoom().getItems().elements(); i.hasMoreElements(); ) {
                         Item an_item = (Item) i.nextElement();
                         if ((an_item.getName().compareToIgnoreCase(selection) == 0)) {
                             p1.addInvItems(an_item);
                             p1.getRoom().removeItem(an_item);
                             System.out.println("\u001B[33m*Item\u001b[0m " + selection + "\u001B[33m added to inventory*\u001b[0m");
-                            Thread.sleep(1000);
                             success = true;
                         }
                     }
-                    if (!success) { System.out.println("You can't take that!"); }
+                    if (!success) {
+                        System.out.println("You can't take that!");
+                    }
+                    Thread.sleep(slowdown);
                     break;
                 case "DROP":
-                    // drop item from inventory
-                    for (Item it : p1.getInvItems()) {
-                        p1.removeInvItems(it);
-                        p1.getRoom().addItem(it);
-                        System.out.println("*\u001B[33mItem removed from inventory*\u001b[0m");
-                        Thread.sleep(1000);
+                    // drop item from inventory and add it to the room
+                    // No matter how I do it I get an java.util.ConcurrentModificationException here ???
+                    for (Item an_item : p1.getInvItems()) {
+                        if (an_item.getName().compareToIgnoreCase(selection) == 0) {
+                            p1.removeInvItems(an_item);
+                            p1.getRoom().addItem(an_item);
+                            success = true;
+                            System.out.println("*\u001B[33mItem removed from inventory*\u001b[0m");
+                        }
                     }
+                    if (!success) {
+                        System.out.println("You can't do that!");
+                    }
+                    Thread.sleep(slowdown);
                     break;
                 case "LOOK":
-                        if (selection.equals("AROUND")) {
-                            System.out.println("\u001B[33mLooking around for details...\u001b[0m\n");
-                            Thread.sleep(2500);
-                            System.out.println("\u001B[33mThe room is empty.\u001b[0m");
-                            System.out.println("\u001B[38;5;199mPress Enter key to return in exploration\u001b[0m");
-                            try{System.in.read();}
-                            catch(Exception e){}
+                    // print all items/things that exist in the room
+                    if (selection.equals("AROUND")) {
+                        System.out.println("\u001B[33mLooking around for details...\u001b[0m\n");
+                        Thread.sleep(slowdown * 2);
+                        System.out.println("\u001B[33mThe room is empty.\u001b[0m");
+                        System.out.println("\u001B[38;5;199mPress Enter key to return in exploration\u001b[0m");
+                        try {
+                            System.in.read();
+                        } catch (Exception ignored) {
                         }
-                        if (selection.equals("INV") || selection.equals("INVENTORY")){
-                            System.out.println(p1.getInvItems());
+                    }
+                    // print all items in the player's inventory
+                    if (selection.equals("INV") || selection.equals("INVENTORY")) {
+                        System.out.println(p1.getInvItems());
 
-                            System.out.println("\u001B[38;5;199mPress Enter key to return in exploration\u001b[0m");
-                            try{System.in.read();}
-                            catch(Exception e){}
+                        System.out.println("\u001B[38;5;199mPress Enter key to return in exploration\u001b[0m");
+                        try {
+                            System.in.read();
+                        } catch (Exception e) {
                         }
+                    }
                     break;
                 case "USE":
                     // use things or items, open doors etc...
-                    for (Enumeration d = p1.getRoom().getDoors().elements(); d.hasMoreElements();) {
+                    for (Enumeration d = p1.getRoom().getDoors().elements(); d.hasMoreElements(); ) {
                         Door a_door = (Door) d.nextElement();
                         if (a_door.getName().compareToIgnoreCase(selection) == 0) {
                             if (a_door.open() == null) {
                                 System.out.println("The door is locked!");
                                 System.out.println("It requires: " + a_door.getRequires().getName());
                                 System.out.println(("Checking inventory for required key."));
-                                Thread.sleep(1500);
                                 // probably there is a better way to do this...
                                 if (p1.getInvItems().contains(a_door.getRequires())) {
                                     System.out.println("You used the " + a_door.getRequires().getName() + " to unlock the door.");
                                     a_door.unlock();
-                                    p1.removeInvItems(a_door.getRequires()); // item is removed from the inventory after use
+                                    // item is removed from the inventory after use
+                                    p1.removeInvItems(a_door.getRequires());
+                                    success = true;
                                 }
                             } else {
                                 // just go through if it's unlocked
+                                System.out.println("You go throug the " + a_door.getName() + "...");
                                 p1.setRoom(a_door.getExit().getLeadsTo());
+                                success =true;
                             }
                         }
                     }
+                    if (!success) { System.out.println("You can't go throug!"); }
+                    Thread.sleep(slowdown);
                     break;
-                case "INTERACT":
-                    // maybe we can use this for some other stuff...
-                    break;
+                case "QUIT":
+                    System.out.println("TERMINATING...");
+                    // should we use the quit variable here or this is fine ???
+                    System.exit(0);
                 default:
+                    // if the command is not recognized tell the player
                     System.out.println("Command " + command + " is not valid.");
                     break;
             }
-        } catch (Exception ArrayIndexOutOfBounds) {
-            System.err.println("Wrong input. The command must be like the examples (GO NORTH, TAKE KEY, etc)");   //temporarily disabled
+        } catch (Exception e) {
+            System.err.println(e);
+            System.err.println("Wrong input. The command must be like the examples (GO NORTH, TAKE KEY, etc)");
         }
+        // switch end ---
     }
 }
